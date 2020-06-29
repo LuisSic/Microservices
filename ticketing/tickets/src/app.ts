7385cc6878c563ab0cae1b8b9a42b4770a67d923
LuisSic <lantoni24@hotmail.com>
2020-06-29 14:40:45 -0600

    Tickets

--- ticketing/tickets/src/app.ts
+++ ticketing/tickets/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
-
 import cookieSession from 'cookie-session';
-
-import { errorHandler, NotFoundError } from '@blackteam/common';
+import { errorHandler, NotFoundError, currentUser } from '@blackteam/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
@@ -15,6 +17,13 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
