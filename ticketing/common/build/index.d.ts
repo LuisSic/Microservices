c782f6948d4ab0d5c75eec5a3d2a04e12e95974f
LuisSic <lantoni24@hotmail.com>
2020-07-08 05:14:09 -0600

    Updates

--- ticketing/common/build/index.d.ts
+++ ticketing/common/build/index.d.ts
@@ -14,3 +14,5 @@ export * from './events/subjects';
 export * from './events/ticket-created-event';
 export * from './events/ticket-updated-event';
 export * from './events/Types/order-status';
+export * from './events/order-created-event';
+export * from './events/order-cancelled-event';
